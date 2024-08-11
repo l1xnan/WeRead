3383243776f5514573abae79bee4be294dee3186
l1xnan <l1xnan@qq.com>
2024-08-11 19:46:25 +0800

    refactor code

--- src-tauri/inject/preload.js
+++ src-tauri/inject/preload.js
@@ -26,49 +26,140 @@ function changeWidth(increse) {
   window.dispatchEvent(event);
 }
 
+const html = (strings, ...placeholders) => {
+  return String.raw(strings, ...placeholders);
+};
+
 window.addEventListener("load", async (_event) => {
   const { invoke } = window.__TAURI__.core;
-  const { listen } = window.__TAURI__.event;
   console.log("execute onload...");
+
   const store = new Store("settings.json");
   isScrolling = (await store.get("is-scrolling")) ?? false;
   scrollSpeed = (await store.get("scroll-speed")) ?? 1;
 
   // 添加内容
-  const btnControls = `
-  <button id='widthIncrease' title="加宽" class='readerControls_item extra-item' style='color:#6a6c6c;cursor:pointer;'>
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"  fill="none"  stroke="currentColor" stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-viewport-wide">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12h-7l3 -3m0 6l-3 -3" /><path d="M14 12h7l-3 -3m0 6l3 -3" /><path d="M3 6v-3h18v3" /><path d="M3 18v3h18v-3" />
-    </svg>
-  </button>
-  <button id='widthDecrease' title="减宽" class='readerControls_item extra-item' style='color:#6a6c6c;cursor:pointer;'>
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-viewport-narrow">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12h7l-3 -3m0 6l3 -3" /><path d="M21 12h-7l3 -3m0 6l-3 -3" /><path d="M9 6v-3h6v3" /><path d="M9 18v3h6v-3" />
-    </svg>
-  </button>
+  const btnControls = html`
+    <button
+      id="widthIncrease"
+      title="加宽"
+      class="readerControls_item extra-item"
+      style="color:#6a6c6c;cursor:pointer;"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-viewport-wide"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M10 12h-7l3 -3m0 6l-3 -3" />
+        <path d="M14 12h7l-3 -3m0 6l3 -3" />
+        <path d="M3 6v-3h18v3" />
+        <path d="M3 18v3h18v-3" />
+      </svg>
+    </button>
+    <button
+      id="widthDecrease"
+      title="减宽"
+      class="readerControls_item extra-item"
+      style="color:#6a6c6c;cursor:pointer;"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-viewport-narrow"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M3 12h7l-3 -3m0 6l3 -3" />
+        <path d="M21 12h-7l3 -3m0 6l-3 -3" />
+        <path d="M9 6v-3h6v3" />
+        <path d="M9 18v3h6v-3" />
+      </svg>
+    </button>
 
-  <button id='controlScroll' title="滚动" class='readerControls_item extra-item' style='color:#6a6c6c;cursor:pointer;'>
-    <svg id="controlScroll__start" style="display:block;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-square-chevrons-down">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8l-3 3l-3 -3" /><path d="M15 13l-3 3l-3 -3" /><path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" />
-    </svg>
-    <svg id="controlScroll__stop" style="display:none;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-player-pause">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /><path d="M14 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
-    </svg>
-  </button>
+    <button
+      id="controlScroll"
+      title="滚动"
+      class="readerControls_item extra-item"
+      style="color:#6a6c6c;cursor:pointer;"
+    >
+      <svg
+        id="controlScroll__start"
+        style="display:block;"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-square-chevrons-down"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M15 8l-3 3l-3 -3" />
+        <path d="M15 13l-3 3l-3 -3" />
+        <path
+          d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z"
+        />
+      </svg>
+      <svg
+        id="controlScroll__stop"
+        style="display:none;"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-player-pause"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path
+          d="M6 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"
+        />
+        <path
+          d="M14 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"
+        />
+      </svg>
+    </button>
 
-  <button id='autoScroll' title="滚动速度" class='readerControls_item extra-item' style='color:#6a6c6c;cursor:pointer;font-size:14px;display:none;'>
-  ×${scrollSpeed}
-  </button>
+    <button
+      id="autoScroll"
+      title="滚动速度"
+      class="readerControls_item extra-item"
+      style="color:#6a6c6c;cursor:pointer;font-size:14px;display:none;"
+    >
+      ×${scrollSpeed}
+    </button>
   `;
 
-  const btnSetting = `
-  <span class="readerTopBar_link_sep"></span>
-  <a id="__setting__" class="readerTopBar_link">设置</a>
+  const btnSetting = html`
+    <span class="readerTopBar_link_sep"></span>
+    <a id="__setting__" class="readerTopBar_link">设置</a>
   `;
 
-  const btnSetting1 = `
-  <span class="navBar_separator"></span>
-  <button id="__setting__" class="navBar_link">设置</button>
+  const btnSetting1 = html`
+    <span class="navBar_separator"></span>
+    <button id="__setting__" class="navBar_link">设置</button>
   `;
 
   const elements = [
@@ -156,17 +247,6 @@ window.addEventListener("load", async (_event) => {
       selBtn.style.opacity = 1;
       windowTop = scrollS;
     }
-  });
-  console.log("pre count =", document.querySelectorAll("pre").length);
-  document.querySelectorAll("pre").forEach((elem) => {
-    // 获取当前的 transform 属性值
-    const transform = elem.style.transform;
-    // 正则表达式匹配 translate 中的第一个值
-    // translate(36px, 2423px) -> translate(0px, 2423px)
-    const regex = /translate\((-?\d+px),/;
-    // 替换为 0px
-    elem.style.transform = transform.replace(regex, "translate(0px,");
-    elem.style.width = "auto";
   });
 });
 
